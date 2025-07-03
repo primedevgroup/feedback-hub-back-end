2890b56f3a18a69262636dbf4fdb8e96b0705b17
AntonyLajes <tonymateuslajes@hotmail.com>
2025-07-03 16:58:24 -0300

    db: remove owner id squad column

---
+++ prisma/migrations/20250703195757_remove_owner_id_squad_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `owner_id` on the `squads` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "squads" DROP COLUMN "owner_id";
