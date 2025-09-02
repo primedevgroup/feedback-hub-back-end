9dde3c9f6c8e24af5ae4c3dd010d9fbc5d9b2717
AntonyLajes <tonymateuslajes@hotmail.com>
2025-09-02 19:11:20 -0300

    FEAT: delete squad by id

--- src/presentations/routes/private/squad.routes.ts
+++ src/presentations/routes/private/squad.routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 import { createSquad } from "@/modules/squads/use-cases/create";
 import { findByUser } from "@/modules/squads/use-cases/find-by-user";
 import { joinSquad } from "@/modules/squads/use-cases/join";
+import { deleteSquad } from "@/modules/squads/use-cases/delete";
 
 export const squadRoutes = async (app: FastifyInstance) => {
   app.post("/", createSquad);
   app.post("/:squadId/join", joinSquad);
   app.get("/find-by-user", findByUser);
+  app.delete("/:squadId/delete", deleteSquad);
 };
